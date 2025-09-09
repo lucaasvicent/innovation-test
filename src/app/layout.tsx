c21219d86a5686681db18fcdde17bb4efe78a891
Lucas Vicente <lucasdev08@hotmail.com>
2025-09-09 11:43:43 -0300

    fix(metadata): correction in icon, descriptions and Open Graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,20 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Innova Brindes',
   description: 'Sistema de produtos Innova Brindes',
-};
+  openGraph: {
+    title: 'Innova Brindes',
+    description: 'Sistema de produtos Innova Brindes!',
+    url: 'https://lp-web-blog.vercel.app/og-image.jpg',
+    siteName: 'Innova Brindes',
+    locale: 'pt-BR',
+    type: 'website',
+  },
+  icons: {
+    icon: '/logo.jpg',
+    shortcut: '/logo.jpg',
+    apple: '/logo.jpg',
+  },
+}
 
 export default function RootLayout({
   children,
